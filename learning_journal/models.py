4a695af685ec794d5a7ff8454b141d4d10d9d016
Tiffany Ku <tiffany.ku3@gmail.com>
2016-02-09 09:51:57 -0800

    created class Entry in modesl.py

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -25,3 +25,18 @@ class MyModel(Base):
     value = Column(Integer)
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+class Entry(Base):
+    __tablename__ = "entries"
+    id = Column(Integer, primary_key=True)
+    title = Column(UnicodeText(255), unique=True, required=True)
+    body = Column(UnicodeText, required=False)
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
+    
+    @classmethod
+
+    def by_id(cls, id):
+
+    @classmethod
+    def all(cls):
